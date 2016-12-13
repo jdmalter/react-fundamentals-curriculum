97fdec1cd9ef171e02404b68174602b39718d506
Jacob Malter <jdmalter@ncsu.edu>
2016-12-12 21:37:32 -0500

    Completed step 7.
    Reused loading component from github-battle.

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,9 +1,14 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var Loading = require('./Loading');
 
 function Forecast(props) {
-   return (
-      <div>Forecast Component</div>
-   )
+   console.log(props);
+   return (props.isLoading === true
+      ? <Loading/>
+      : <div>
+         Forecast Component
+      </div>)
 }
 
 module.exports = Forecast;
--- app/containers/ForecastContainer.js
+++ app/containers/ForecastContainer.js
@@ -1,9 +1,24 @@
 var React = require('react');
 var Forecast = require('../components/Forecast');
+var getForecast = require('../util/openWeatherMapHelper').getForecast;
 
 var ForecastContainer = React.createClass({
+   getInitialState: function () {
+      return {isLoading: true, forecast: {}}
+   },
+   componentDidMount: function () {
+      this.requestForecast(this.props.routeParams.city)
+   },
+   componentWillReceiveProps: function (props) {
+      this.requestForecast(props.routeParams.city)
+   },
+   requestForecast: function (city) {
+      getForecast(city).then(function (forecast) {
+         this.setState({isLoading: false, forecast: forecast});
+      }.bind(this))
+   },
    render: function () {
-      return (<Forecast/>)
+      return (<Forecast city={this.props.routeParams.city} isLoading={this.state.isLoading} forecast={this.state.forecast}/>)
    }
 });
 
